84a3db853c9dff2e851b50b946e0991a829690c2
JoseCalel10 <josepablo2915@gmail.com>
2023-11-03 14:47:05 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data = [
+    {
+        id: 1,
+        img:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        nameProduct:'Zapatos Nike',
+        price: 90,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1585032767761-878270336a0b?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        nameProduct: 'Playera Nike',
+        price: 50,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1608231387042-66d1773070a5?auto=format&fit=crop&q=80&w=1074&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        nameProduct: 'Zapatos Puma',
+        price: 80,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: 'https://images.unsplash.com/photo-1607522370275-f14206abe5d3?auto=format&fit=crop&q=80&w=1121&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        nameProduct: 'Zapatos Converse',
+        price: 60,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: 'https://images.unsplash.com/photo-1599839614778-a0ee8a16f2a9?auto=format&fit=crop&q=80&w=987&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        nameProduct: 'Sudadero Adidas',
+        price: 50,
+        quantity: 1,
+    },
+
+
+];
